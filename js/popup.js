aab218f491f7b78c859676bcd9fe60599cedcf3f
Marcus Greer <marcusgreer96@gmail.com>
2019-04-22 02:39:52 -0400

    fixed small error in popup.js

--- js/popup.js
+++ js/popup.js
@@ -226,7 +226,7 @@ window.onload = function(){
 
 	chrome.tabs.query({ active: true, currentWindow: true}, 
     	function (tabs) {
-    		chrome.tabs.sendMessge(
+    		chrome.tabs.sendMessage(
     			tabs[0].id,
     			{
     				from: 'popup',
